a529fecd71d4b0006d6d6a8b535834f0f6acb8c3
narek07n <narek07n@gmail,com>
2024-04-24 16:04:12 +0400

    added product item page

--- front/src/app/(client)/products/[product]/page.tsx
+++ front/src/app/(client)/products/[product]/page.tsx
@@ -1,5 +1,45 @@
 "use client";
 
+import { Box } from "@mui/material";
+import { Image } from "@/components/shared/image";
+
+import styles from "./styles.module.scss";
+import { imagesPNG } from "@/utils/constants/imagesSRC";
+
 export default function Product({ params }: { params: { product: string } }) {
-  return <>Product: {params.product}</>;
+  const name = "Product 1";
+  const price = 0; // get by product id from params
+  const descr =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea laboriosam pariatur accusamus natus sint sit ipsum ab, labore repudiandae sapiente voluptatum in dolorum odio quidem quod? Exercitationem similique modi veritatis.";
+  return (
+    <Box className={styles.container}>
+      <Box className={styles.left}>
+        <Image className={styles.image} src={imagesPNG.logo} alt={name} />
+      </Box>
+      <Box className={styles.right}>
+        <span className={styles.name}>{name}</span>
+        <span className={styles.price}>Price ${price}</span>
+        <Box className={styles.bottom}>
+          <p className={styles.descr}>{descr}</p>
+          <Box className={styles.images}>
+            <Image
+              className={styles.imagesItem}
+              src={imagesPNG.logo}
+              alt={name}
+            />
+            <Image
+              className={styles.imagesItem}
+              src={imagesPNG.logo}
+              alt={name}
+            />
+            <Image
+              className={styles.imagesItem}
+              src={imagesPNG.logo}
+              alt={name}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
